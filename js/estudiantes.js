6b9936f8e495818f1f9c972ca0dbf40738803b0d
ritdaniel <ritdaniel@hotmail.com>
2021-11-23 12:58:23 -0300

    api

---
+++ js/estudiantes.js
@@ -0,0 +1,22 @@
+function mostrarEstudiantes(mostrar) {
+    $('#datos').empty();
+    const URLGET = "http://hp-api.herokuapp.com/api/characters/students";
+    $.get(URLGET).done(function(respuesta, estado) {
+
+        if (estado === "success") {
+            let listadoEstudiantes = respuesta;
+            listadoEstudiantes.forEach(estudiantes => {
+                if (mostrar === estudiantes.house) {
+                    $("#datos").append(`<div class="productos">
+                <h1 class="cajaProductos__title--orange">${estudiantes.name}</h1>
+                <img src="${estudiantes.image}"alt = "${estudiantes.name}">
+                <h3 class ="cajaProductos__text--grey" > ${estudiantes.actor}</h3>
+                <p class ="descripcion" >${estudiantes.house}</p>
+                </div>`);
+                }
+            });
+        }
+    });
+}
+
+//mostrarEstudiantes()
