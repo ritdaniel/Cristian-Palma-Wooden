610e04590472ecd52899c516a823df7bb06b98d6
ritdaniel <ritdaniel@hotmail.com>
2021-12-09 10:32:21 -0300

    mejorado

--- js/carrito.js
+++ js/carrito.js
@@ -133,7 +133,7 @@ tituloPrecio.innerText = "0";
 
 // creamos el elemento tituloTotalUnidades (cantidad productos en el carro) para poder imprimir dentro del id correspondiente    
 let tituloTotalUnidades = document.getElementById("Cantidad");
-tituloTotalUnidades.style.textAlign = "right"; // agregamos estilo (texto al lado derecho)
+//tituloTotalUnidades.style.textAlign = "right"; // agregamos estilo (texto al lado derecho)
 tituloTotalUnidades.innerText = "0";
 // recuperamos el carro cuando la persona se cambio de pagina
 if (localStorage.getItem('carro')) {
